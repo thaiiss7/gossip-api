b0e5c679138473217e9e20d88f896dd2ed3ba2bb
thaiiss7 <thaismichel7@gmail.com>
2025-07-28 13:33:57 -0300

    add models

---
+++ Models/Post.cs
@@ -0,0 +1,12 @@
+namespace Gossip.Models;
+
+public class Post
+{
+    public Guid ID { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Guid ProfileID { get; set; }
+
+    public Profile Author { get; set; }
+}
