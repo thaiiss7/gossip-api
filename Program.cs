d68892aea76c91fbebfea3b96e3ac60749aca739
thaiiss7 <thaismichel7@gmail.com>
2025-07-28 14:50:54 -0300

    add tests

--- Program.cs
+++ Program.cs
@@ -1,17 +1,72 @@
+using System.Text;
+using Gossip.Endpoints;
 using Gossip.Models;
 using Gossip.Services;
+using Gossip.Services.JWT;
+using Gossip.Services.Password;
 using Gossip.Services.Profiles;
+using Gossip.UseCases;
+using Gossip.UseCases.DeletePost;
+using Gossip.UseCases.GetProfileData;
+using Gossip.UseCases.Login;
+using Gossip.UseCases.PublishPost;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<GossipDbContext>(options => {
+builder.Services.AddDbContext<GossipDbContext>(options =>
+{
     var sqlConn = Environment.GetEnvironmentVariable("SQL_CONNECTION");
     options.UseSqlServer(sqlConn);
 });
 
+builder.Services.AddTransient<IPasswordService, PBKDF2PasswordService>();
 builder.Services.AddTransient<IProfilesService, EFProfileService>();
+builder.Services.AddSingleton<IJWTService, JWTService>();
+
+builder.Services.AddTransient<LoginUseCase>();
+builder.Services.AddTransient<DeletePostUseCase>();
+builder.Services.AddTransient<CreateProfileUseCase>();
+builder.Services.AddTransient<PublishPostUseCase>();
+builder.Services.AddTransient<GetProfileDataUseCase>();
+
+var jwtSecret = Environment.GetEnvironmentVariable("JWT_SECRET");
+var keyBytes = Encoding.UTF8.GetBytes(jwtSecret);
+var key = new SymmetricSecurityKey(keyBytes);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidIssuer = "fofoquinha-app",
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero,
+            IssuerSigningKey = key,
+        };
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.ConfigureAuthEndpoints();
+app.ConfigurePostEndpoints();
+app.ConfigureProfileEndpoints();
 
 app.Run();
---
+++ Tests/AuthTest.cs
@@ -0,0 +1,50 @@
+using Gossip.Models;
+using Gossip.Services.JWT;
+using Gossip.Services.Password;
+using Gossip.Services.Profiles;
+using Gossip.UseCases.Login;
+using Moq;
+using Xunit;
+
+namespace Gossip.Tests;
+
+public class AuthTest
+{
+    [Theory]
+    [InlineData("minhasenhaA", "minhasenhaA", true)]
+    [InlineData("1232132132131", "asdafafafd", false)]
+    [InlineData("", "a", false)]
+    [InlineData("acomplexpasswordhaha123", "acomplexpasswordhaha123", true)]
+    [InlineData(null, "acomplexpasswordhaha123", false)]
+    public void TestPasswordHash(string senha1, string senha2, bool result)
+    {
+        var hasher = new PBKDF2PasswordService();
+        var hash = hasher.Hash(senha1);
+        var ok = hasher.Compare(senha2, hash);
+        Assert.Equal(result, ok);
+    }
+
+    [Fact]
+    public async Task TestLogin()
+    {
+        var profileService = new Mock<IProfilesService>();
+        profileService.Setup(s => s.FindByLogin("trevis"))
+            .ReturnsAsync(new Profile {
+                Username = "trevis",
+                Password = "hash_da_senha"
+            });
+        var passworService = new Mock<IPasswordService>();
+        passworService.Setup(s => s.Compare("cristianS2", "hash_da_senha"))
+            .Returns(true);
+        var jwtSevice = new Mock<IJWTService>();
+
+        var useCase = new LoginUseCase(
+            profileService.Object,
+            passworService.Object,
+            jwtSevice.Object
+        );
+
+        var result = await useCase.Do(new LoginPayload("trevis", "cristianS2"));
+        Assert.True(result.IsSuccess);
+    }
+}
